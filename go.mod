964e9fb5f8e49cad202e4259ce6464d1abde387e
Ma He <qmahe@live.com>
2023-12-11 20:01:33 +0800

    add docs and users base type

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module attendance.com
 
 go 1.21.1
+
+require golang.org/x/crypto v0.16.0
--- src/user/users.go
+++ src/user/users.go
@@ -1,6 +1,38 @@
+/*
+Package users provides functionalities for performing requests specific to users.
+
+It includes functionalities for:
+  - Login/Logout
+  - User struct
+  - All methods relevant to users
+*/
 package users
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User represents the metadata of a user
+type User struct {
+	ID       string
+	Password []byte
+	First    string
+	Last     string
+}
+
+// Variables that holds the user states
+var (
+	mapUsers    = map[string]User{}
+	mapSessions = map[string]string{}
+)
+
+func init() {
+	// init special access for admin
+	bPassword, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.MinCost)
+	mapUsers["admin"] = User{"admin", bPassword, "admin", "admin"}
+}
 
 func GetUser() {
 	fmt.Println("USERS")
