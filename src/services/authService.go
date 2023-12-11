e196f04bf186a1acc3dcc252613f70ba5391c6e9
Ma He <qmahe@live.com>
2023-12-11 20:34:53 +0800

    move users to auth service

--- src/services/authService.go
+++ src/services/authService.go
@@ -2,3 +2,119 @@
 Package services provides business logic for performing requests specific to each endpoint.
 */
 package services
+
+import (
+	"net/http"
+
+	"attendance.com/templates"
+	uuid "github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User represents the metadata of an authenticated user
+type User struct {
+	ID       string
+	Password []byte
+	First    string
+	Last     string
+}
+
+// Variables that holds the auth states
+var (
+	mapUsers    = map[string]User{}
+	mapSessions = map[string]string{}
+)
+
+func init() {
+	// init special access for admin
+	bPassword, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.MinCost)
+	mapUsers["admin"] = User{"admin", bPassword, "admin", "admin"}
+}
+
+func Login(res http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	// process form submission
+	if req.Method == http.MethodPost {
+		username := req.FormValue("username")
+		password := req.FormValue("password")
+		// check if user exist with username
+		myUser, ok := mapUsers[username]
+		if !ok {
+			http.Error(res, "Username and/or password do not match", http.StatusUnauthorized)
+			return
+		}
+		// Matching of password entered
+		err := bcrypt.CompareHashAndPassword(myUser.Password, []byte(password))
+		if err != nil {
+			http.Error(res, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+		// create session
+		id := uuid.NewV4()
+		myCookie := &http.Cookie{
+			Name:  "myCookie",
+			Value: id.String(),
+		}
+		http.SetCookie(res, myCookie)
+		mapSessions[myCookie.Value] = username
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	templates.Tpl.ExecuteTemplate(res, "login.gohtml", nil)
+}
+
+func Logout(res http.ResponseWriter, req *http.Request) {
+	if !alreadyLoggedIn(req) {
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+	myCookie, _ := req.Cookie("myCookie")
+	// delete the session
+	delete(mapSessions, myCookie.Value)
+	// remove the cookie
+	myCookie = &http.Cookie{
+		Name:   "myCookie",
+		Value:  "",
+		MaxAge: -1,
+	}
+	http.SetCookie(res, myCookie)
+
+	http.Redirect(res, req, "/", http.StatusSeeOther)
+}
+
+func alreadyLoggedIn(req *http.Request) bool {
+	myCookie, err := req.Cookie("myCookie")
+	if err != nil {
+		return false
+	}
+	username := mapSessions[myCookie.Value]
+	_, ok := mapUsers[username]
+	return ok
+}
+
+func GetUser(res http.ResponseWriter, req *http.Request) User {
+	// get current session cookie
+	myCookie, err := req.Cookie("myCookie")
+	if err != nil {
+		id := uuid.NewV4()
+		myCookie = &http.Cookie{
+			Name:  "myCookie",
+			Value: id.String(),
+		}
+
+	}
+	http.SetCookie(res, myCookie)
+
+	// if the user exists already, get user
+	var myUser User
+	if username, ok := mapSessions[myCookie.Value]; ok {
+		myUser = mapUsers[username]
+	}
+
+	return myUser
+}
