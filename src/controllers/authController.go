667edf36ae98e6873479db7f3add0bcf68ba3aec
Ma He <qmahe@live.com>
2023-12-11 20:01:46 +0800

    add auth controller

---
+++ src/controllers/authController.go
@@ -0,0 +1,29 @@
+/*
+Package controllers provides all the controllers for each specific use case
+*/
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func AuthController(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(os.Stdout, "control:: "+r.URL.Path+"\n")
+
+	path := strings.TrimPrefix(r.URL.Path, "/auth")
+
+	fmt.Fprint(os.Stdout, "subpath:: "+path+"\n")
+
+	switch path {
+	case "":
+		fmt.Fprint(w, "working auth")
+	case "/test":
+		fmt.Fprint(w, "hehe")
+	default:
+		http.NotFound(w, r)
+	}
+
+}
