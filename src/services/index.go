447af4baa04949faee7449bfb6f9a27d14e8f6cc
Ma He <qmahe@live.com>
2023-12-13 11:49:30 +0800

    add css to templates

--- src/services/index.go
+++ src/services/index.go
@@ -10,7 +10,7 @@ import (
 func Index(w http.ResponseWriter, r *http.Request) {
 	currUser := GetUser(w, r)
 
-	err := templates.Tpl.ExecuteTemplate(w, "index.gohtml", currUser)
+	err := templates.Tpl.ExecuteTemplate(w, "index", currUser)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		log.Fatal("Template execution error:", err)
--- src/templates/templates.go
+++ src/templates/templates.go
@@ -3,10 +3,17 @@ Package templates initializes all the templates for use in services.
 */
 package templates
 
-import "html/template"
+import (
+	"html/template"
+
+	"attendance.com/src/logger"
+)
 
 var Tpl *template.Template
 
 func init() {
+	logger.Println("Initializing templates...")
 	Tpl = template.Must(template.ParseGlob("./templates/*.gohtml"))
+	logger.Println("Templates ready!")
+
 }
