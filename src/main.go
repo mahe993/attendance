2f1e053498c3638fb3412eb877fa1c490667f50d
Ma He <qmahe@live.com>
2023-12-13 13:19:10 +0800

    style templates

--- src/main.go
+++ src/main.go
@@ -23,6 +23,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", router.Routes)
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./templates/css"))))
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	logger.Println("Server listening at port :5332...")
 	log.Fatal(http.ListenAndServe(":5332", nil))
